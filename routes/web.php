3dde7c904dca0f57cfefcfdde25510450c967fde
putriauliam <ptriauliam61@gmail.com>
2023-10-27 07:24:45 +0700

    Video 3 -  Struktur Folder, Routes & View

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Putri Aulia Maulidina",
+        "email" => "putri.213040055@unpas.ac.id",
+        "image" => "putri.jpeg"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });

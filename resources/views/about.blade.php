52de4986266c055509cd0109353e04a7e82f003b
putriauliam <ptriauliam61@gmail.com>
2023-11-01 20:43:40 +0700

    Video 5 - Model, Collection & Controller

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,6 +3,6 @@
 @section('container')  
     <h3>{{ $name }}</h3>
     <p>{{ $email }}</p>
-    <img src="img/{{ $image }}" alt="{{ $name }}" width="200">
+    <img src="img/{{ $image }}" alt="{{ $name }}" width="200" class="img-thumbnail rounded-circle">
 @endsection    
     
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -29,56 +32,8 @@ Route::get('/about', function () {
 });
 
 
-Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug"=> "judul-post-pertama",
-            "author" => "Putri Aulia Maulidina",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum consequatur delectus quaerat sit impedit laboriosam asperiores ullam, sed repudiandae distinctio recusandae ipsa quis ratione possimus accusantium provident quia dignissimos? Sed fugiat porro provident necessitatibus asperiores hic, tempore doloremque quaerat dolorum atque inventore! Asperiores quidem cum, obcaecati excepturi a beatae iure tenetur placeat exercitationem aspernatur id ex! Doloremque hic provident officia numquam magnam laborum libero quidem aliquid eos at nulla architecto sequi dignissimos labore autem facilis rem, fugiat pariatur alias! Illum!"
-        ],
-    
-        [
-            "title" => "Judul Post Kedua",
-            "slug"=> "judul-post-kedua",
-            "author" => "Maxine Mayfield",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, corporis aliquid ipsam ullam accusamus, cum tempora vero fugiat laudantium tenetur hic doloremque repellat deserunt obcaecati non, delectus nam? Cum amet non ea maxime fuga tempora sed repellendus asperiores iure eos maiores ducimus, laudantium aliquam est neque blanditiis laborum esse impedit reiciendis vero! Maiores vitae nisi consequatur numquam placeat neque est nam necessitatibus odio obcaecati ad optio, veniam nostrum inventore tempore porro dolorum quod, velit accusantium eaque! Vero exercitationem quae explicabo delectus sequi sed mollitia sapiente maxime laudantium, officia officiis necessitatibus error rem ad et! Voluptatem nulla rem quod a velit?"
-        ]
-    ];
-
-    return view('posts', [
-        "title" => "Posts",
-        "posts" => $blog_posts
-    ]);
-});
+Route::get('/blog', [PostController::class, 'index']);
 
 // halaman single post
-Route::get('posts/{slug}', function($slug) {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug"=> "judul-post-pertama",
-            "author" => "Putri Aulia Maulidina",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum consequatur delectus quaerat sit impedit laboriosam asperiores ullam, sed repudiandae distinctio recusandae ipsa quis ratione possimus accusantium provident quia dignissimos? Sed fugiat porro provident necessitatibus asperiores hic, tempore doloremque quaerat dolorum atque inventore! Asperiores quidem cum, obcaecati excepturi a beatae iure tenetur placeat exercitationem aspernatur id ex! Doloremque hic provident officia numquam magnam laborum libero quidem aliquid eos at nulla architecto sequi dignissimos labore autem facilis rem, fugiat pariatur alias! Illum!"
-        ],
-    
-        [
-            "title" => "Judul Post Kedua",
-            "slug"=> "judul-post-kedua",
-            "author" => "Maxine Mayfield",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, corporis aliquid ipsam ullam accusamus, cum tempora vero fugiat laudantium tenetur hic doloremque repellat deserunt obcaecati non, delectus nam? Cum amet non ea maxime fuga tempora sed repellendus asperiores iure eos maiores ducimus, laudantium aliquam est neque blanditiis laborum esse impedit reiciendis vero! Maiores vitae nisi consequatur numquam placeat neque est nam necessitatibus odio obcaecati ad optio, veniam nostrum inventore tempore porro dolorum quod, velit accusantium eaque! Vero exercitationem quae explicabo delectus sequi sed mollitia sapiente maxime laudantium, officia officiis necessitatibus error rem ad et! Voluptatem nulla rem quod a velit?"
-        ]
-    ];
-
-    $new_post = [];
-    foreach($blog_posts as $post) {
-        if($post["slug"] === $slug) {
-            $new_post = $post;
-        }
-    }
-
-    return view('post', [
-        "title" => "Single Post",
-        "post" => $new_post
-    ]);
-});
+Route::get('posts/{slug}', [PostController::class, 'show']);
+ 

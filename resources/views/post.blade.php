9369f7b72fecdcce36eb202630458d964e4ae455
putriauliam <ptriauliam61@gmail.com>
2023-11-03 10:53:33 +0700

    Video 8 - Post Category & Eloquent Relationship

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,6 +3,8 @@
 @section('container')
     <article>
         <h1 class="mb-5">{{ $post->title }}</h1>
+
+        <p>By. Putri Aulia Maulidina in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
         
         {!! $post->body !!}
     </article>
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\PostController;
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
-
+use App\Models\Category;
 
 /*
 |--------------------------------------------------------------------------
@@ -36,4 +36,22 @@ Route::get('/blog', [PostController::class, 'index']);
 
 // halaman single post
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
+
+Route::get('/categories/', function(){
+    return view('categories', [
+        'title' => 'Post Categories',
+        'categories' => Category::all()
+
+    ]);
+});
+
+
+Route::get('/categories/{category:slug}', function(Category $category){
+    return view('category', [
+        'title' => $category->name,
+        'posts' => $category->posts,
+        'category' => $category->name 
+
+    ]);
+});
  

f74dccffb5c39bb0e7232d06372d6ec4caa45fdc
putriauliam <ptriauliam61@gmail.com>
2023-11-04 18:24:54 +0700

    Video  14 - View Login & Registration

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,11 @@
 <?php
 
-use App\Http\Controllers\PostController;
-use App\Models\Post;
-use Illuminate\Support\Facades\Route;
 use App\Models\Category;
-use App\Models\User;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -41,7 +42,7 @@ Route::get('/blog', [PostController::class, 'index']);
 // halaman single post
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 
-Route::get('/categories/', function(){
+Route::get('/categories', function(){
     return view('categories', [
         'title' => 'Post Categories',
         'active' => 'categories',
@@ -50,6 +51,8 @@ Route::get('/categories/', function(){
     ]);
 });
 
+Route::get('/login', [LoginController::class, 'index']);
+Route::get('/register', [RegisterController::class, 'index']);
 
 
- 
+

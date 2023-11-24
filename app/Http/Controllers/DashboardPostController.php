2a9b397d2bb44a3b5e2fa52bef7b76ab48248800
putriauliam <ptriauliam61@gmail.com>
2023-11-24 22:36:57 +0700

    Video 20 - Update & Delete Post

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -55,6 +55,9 @@ class DashboardPostController extends Controller
      */
     public function show(Post $post)
     {
+        if($post->author->id !== auth()->user()->id) {
+            abort(403);
+       }
         return view('dashboard.posts.show', [
             'post' => $post
         ]);
@@ -65,7 +68,14 @@ class DashboardPostController extends Controller
      */
     public function edit(Post $post)
     {
-        //
+        if($post->author->id !== auth()->user()->id) {
+            abort(403);
+        }
+
+        return view('dashboard.posts.edit',[
+            'post' => $post,
+            'categories' => Category::all()
+        ]);
     }
 
     /**
@@ -73,7 +83,25 @@ class DashboardPostController extends Controller
      */
     public function update(Request $request, Post $post)
     {
-        //
+        $rules = [
+            'title' => 'required|max:255',
+            'category_id' => 'required',
+            'body' => 'required'
+       ];
+
+       if($request->slug != $post->slug) {
+            $rules['slug'] = 'required|unique:posts';
+       }
+
+       $validatedData = $request->validate($rules);
+
+       $validatedData['user_id'] = auth()->user()->id;
+       $validatedData['excerpt'] =  Str::limit(strip_tags($request->body), 200);
+
+       Post::where('id', $post->id)
+            ->update($validatedData);
+
+       return redirect('/dashboard/posts')->with('success', 'Post has been updated!');
     }
 
     /**
@@ -81,7 +109,9 @@ class DashboardPostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        Post::destroy($post->id);
+       
+       return redirect('/dashboard/posts')->with('success', 'Post has been deleted!');
     }
 
     public function checkSlug(Request $request)
